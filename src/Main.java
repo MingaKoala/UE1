753e91b361779de15a22e882dcd322c9269c7734
Oltersdorf Benedikt <1810653774@stud.fh-kufstein.ac.at>
2018-10-11 10:55:06 +0200

    Jacky Cola schmeckt gut

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        String vorname = "Bene";
+        int alter = (int) 24.3;
+        System.out.println("Hello "+vorname+" du bist "+alter);
+    }
+}
